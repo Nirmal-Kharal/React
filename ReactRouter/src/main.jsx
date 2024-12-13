0454ad17140bdf3d769c1c790198c453c64d4265
Nirmal-Kharal <nirmalkharal40@gmail.com>
2024-12-13 20:51:59 +0545

    feat/BasicRouting

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Layout from './Layout.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout/>} >
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+<RouterProvider router={ router } />
   </StrictMode>,
 )
